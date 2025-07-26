c316a1e7dad9c58d4e98377e44e556819fd7a7dc
Dylan Senasson <dylan.senasson@outlook.fr>
2025-07-26 08:25:26 +0200

    Implémentation d'une interface UserService.java pour UserServiceImpl

--- paymybuddy/src/main/java/com/paymybuddy/service/UserService.java
+++ paymybuddy/src/main/java/com/paymybuddy/service/UserService.java
@@ -1,36 +1,8 @@
 package com.paymybuddy.service;
 
-import com.paymybuddy.exception.EmailConflictException;
-import com.paymybuddy.exception.UsernameConflictException;
 import com.paymybuddy.model.DTO.RegisterRequest;
-import com.paymybuddy.model.User;
-import com.paymybuddy.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    public void registerUser(RegisterRequest request) {
-        if(userRepository.findByEmail(request.getEmail()).isPresent()) {
-            throw new EmailConflictException("Email déjà utilisé", "register", request);
-        }
-        if(userRepository.findByUsername(request.getUserName()).isPresent()) {
-            throw new UsernameConflictException("UserName déjà utilisé", "register", request);
-        }
-
-        User user = new User();
-
-        user.setUsername(request.getUserName());
-        user.setEmail(request.getEmail());
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-
-        userRepository.save(user);
-    }
+    void registerUser(RegisterRequest request);
 }
