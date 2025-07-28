2851bbd88486c3905aa08eaf92e21a499939f8a4
Dylan Senasson <dylan.senasson@outlook.fr>
2025-07-28 18:01:10 +0200

    Simplification du binding du destinataire dans TransactionRequest
    - Remplacement du champ userReceiver (type User) par userReceiverId (type long) dans le DTO TransactionRequest.
    - Permet d'assurer le binding correct du formulaire Thymeleaf, qui ne gère pas nativement les objets complexes, mais seulement les valeurs simples (id).
    - L'objet User complet est désormais récupéré en service à partir de cet identifiant lors de l'enregistrement de la transaction.

--- paymybuddy/src/main/java/com/paymybuddy/model/DTO/TransactionRequest.java
+++ paymybuddy/src/main/java/com/paymybuddy/model/DTO/TransactionRequest.java
@@ -1,6 +1,5 @@
 package com.paymybuddy.model.DTO;
 
-import com.paymybuddy.model.User;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -10,7 +9,8 @@ import java.math.BigDecimal;
 @Data
 public class TransactionRequest {
     @NotNull(message = "receiver is required")
-    private User userReceiver;
+    private long userReceiverId;
+
     private String description;
 
     @NotNull(message = "amount is required")
