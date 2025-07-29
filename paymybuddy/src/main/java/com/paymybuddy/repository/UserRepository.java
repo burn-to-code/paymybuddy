b4afd1fdf9caafc1ff5122b219da394de7df9d28
Dylan Senasson <dylan.senasson@outlook.fr>
2025-07-29 16:46:37 +0200

    refactor : mise à jour des méthodes du UserRepository pour optimiser la récupération des connexions
    - Remplacement de la méthode `findByEmailWithConnections` par `findByIdWithConnections`.
    - La nouvelle méthode utilise l’ID utilisateur comme paramètre au lieu de l’email.
    - Requête JPQL modifiée : jointure fetch sur `connections` basée sur l’ID de l’utilisateur.
    - Suppression de la méthode `findByEmailWithConnections` pour éviter la redondance.

--- paymybuddy/src/main/java/com/paymybuddy/repository/UserRepository.java
+++ paymybuddy/src/main/java/com/paymybuddy/repository/UserRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUsername(String username);
+
     Optional<User> findByEmail(String email);
 
-    @Query("SELECT u FROM User u LEFT JOIN FETCH u.connections WHERE u.email = :email")
-    Optional<User> findByEmailWithConnections(String email);
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.connections WHERE u.id = :id")
+    Optional<User> findByIdWithConnections(Long id);
 }
+
