4b81d0b08a6712a42e06f4621223c32934a50edf
Dylan Senasson <dylan.senasson@outlook.fr>
2025-07-30 14:04:30 +0200

    Refactor : Ajout de notEmpty sut le mot de passe et userName dans le dto RegisterRequest.java

--- paymybuddy/src/main/java/com/paymybuddy/model/DTO/RegisterRequest.java
+++ paymybuddy/src/main/java/com/paymybuddy/model/DTO/RegisterRequest.java
@@ -2,12 +2,14 @@ package com.paymybuddy.model.DTO;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
 
     @NotBlank(message = "UserName is required")
+    @NotEmpty(message = "UserName is required")
     private String userName;
 
     @NotBlank(message = "Email is required")
@@ -15,5 +17,6 @@ public class RegisterRequest {
     private String email;
 
     @NotBlank(message = "Password is required")
+    @NotEmpty(message = "Password is required")
     private String password;
 }
