da8172b8ef30b481df388d12ddc669086ac9ec5b
Dylan Senasson <dylan.senasson@outlook.fr>
2025-08-11 16:42:28 +0200

    Suppression d'import inutile.

--- paymybuddy/src/main/java/com/paymybuddy/controller/AddRelationController.java
+++ paymybuddy/src/main/java/com/paymybuddy/controller/AddRelationController.java
@@ -1,14 +1,10 @@
 package com.paymybuddy.controller;
-
 import com.paymybuddy.model.User;
 import com.paymybuddy.service.SecurityUtils;
 import com.paymybuddy.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
