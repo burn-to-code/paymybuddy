a453f53da6d1d89642a5fbda1774698b0f3bc6da
Dylan Senasson <dylan.senasson@outlook.fr>
2025-08-11 16:44:50 +0200

    refactor: simplification de CustomOAuth2FailureHandler en supprimant ServletException
    - Suppression de l'import jakarta.servlet.ServletException et de la déclaration de l'exception dans la méthode onAuthenticationFailure
    - Cette méthode ne lance plus ServletException, ce qui simplifie la signature et le code
    - Aucun impact fonctionnel attendu, uniquement un nettoyage du code

--- paymybuddy/src/main/java/com/paymybuddy/security/CustomOAuth2FailureHandler.java
+++ paymybuddy/src/main/java/com/paymybuddy/security/CustomOAuth2FailureHandler.java
@@ -1,6 +1,5 @@
 package com.paymybuddy.security;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
@@ -12,7 +11,7 @@ import java.nio.charset.StandardCharsets;
 
 public class CustomOAuth2FailureHandler implements AuthenticationFailureHandler {
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
         String errorMessage = URLEncoder.encode(exception.getMessage(), StandardCharsets.UTF_8);
         response.sendRedirect("/login?error=" + errorMessage);
     }
